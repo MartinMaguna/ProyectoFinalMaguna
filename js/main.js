ec63863dd4e453f7682cdb4dad8b60107b99072a
Martin Maguna <martinmaguna33@gmail.com>
2023-10-19 16:25:42 -0300

    segundo commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+localStorage.setItem("iva", 21);
+localStorage.setItem
+
+
+let nombre= prompt("Hola Martín, sos vos?");
+
+if (nombre=="si"){ 
+alert("Qué diceeeee tinchooo");
+document.write("Bueno, seguimos");
+}else{
+    alert("Quién sos?");
+    document.write("Decime, quién carajo sos?");
+}
+
+ 
+alert("Agregada a lista de favoritos") 
+
+    
